2f6c3401a6879773dafd674f502776ba1b47fb14
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-12-08 14:07:50 -0600

    Added essentials for express app

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-var http = require('http').Server(app);
-var path = require('path');
-var app = require('express')();
-var io = require('socket.io')(http);
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var pem = require('pem');
 
-var LISTENING_PORT = 3000
+pem.createCertificate({days:365, selfSigned:true}, function(err, keys){
+    var app = express();
 
-app.get('/', function(req, res) {
-    //res.sendFile('index.html', { root: path.join(__dirname + '/static/') });
-    res.sendFile(path.join(__dirname + '/static/index.html'));
-});
+    app.get('/', function (req, res) {
+        var options = {
+            root: __dirname + '/public/',
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
+        var fileName = 'index.html';
+        res.sendFile(fileName, options, function (err) {
+            if (err) {
+              console.log(err);
+              res.status(err.status).end();
+            }
+            else {
+              console.log('Sent:', fileName);
+            }
+        });
+    });
 
-http.listen(LISTENING_PORT, function() {
-    console.log('listening on *:' + LISTENING_PORT);
+    http.createServer(app).listen(3000);
+    https.createServer({key: keys.serviceKey, cert: keys.certificate},
+        app).listen(3001);
+    app.listen = function() {
+        var server = http.createServer(this);
+        return server.listen.apply(server, arguments);
+    };
 });
